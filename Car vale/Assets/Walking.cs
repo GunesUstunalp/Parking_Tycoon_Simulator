0cfe9d9d24edf685b3a0778bd05f299d4882f846
rangagunes <gunesustunalp@hotmail.com>
2020-08-28 02:26:56 +0300

    Animation added

---
+++ Car vale/Assets/Walking.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+
+public class Walking : MonoBehaviour
+{   
+    Animator animator;
+    public float targetX, targetZ;
+    public float speed;
+    Vector3 currentPos;
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        speed = 1;
+    }
+
+    void Update()
+    {
+        currentPos = transform.position;
+
+        double distance = Math.Sqrt(Math.Pow((targetX - currentPos.x), 2)
+                + Math.Pow((targetZ - currentPos.z), 2));
+        Debug.Log(distance);
+
+        if (distance > 0.1)
+        {
+            animator.SetBool("WalkingTrigger", true);
+
+            double slopeZ = (targetZ - currentPos.z) / distance;
+            double slopeX = (targetX - currentPos.x) / distance;
+
+            transform.position += new Vector3((float)(Time.deltaTime * speed * slopeX), transform.position.y , (float)(Time.deltaTime* speed * slopeZ));
+
+        }
+        else
+            animator.SetBool("WalkingTrigger", false);
+
+    }
+}
